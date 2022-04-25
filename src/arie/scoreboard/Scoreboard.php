43a3f0a70941b7349de134d2014444128c450743
NTT1906 <nttispay@gmail.com>
2022-04-25 19:57:01 +0700

    free time :D

--- src/arie/scoreboard/Scoreboard.php
+++ src/arie/scoreboard/Scoreboard.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ * Copyright (c) 2022 Arisify
+ *
+ * This program is freeware, so you are free to redistribute and/or modify
+ * it under the conditions of the MIT License.
+ *
+ *  /\___/\
+ *  )     (     @author Arisify
+ *  \     /
+ *   )   (      @link   https://github.com/Arisify
+ *  /     \     @license https://opensource.org/licenses/MIT MIT License
+ *  )     (
+ * /       \
+ * \       /
+ *  \__ __/
+ *     ))
+ *    //
+ *   ((
+ *    \)
+*/
 declare(strict_types=1);
 
 namespace arie\scoreboard;
